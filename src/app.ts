f14082e9fee84af69d4d703ceaf3fdca82ef9114
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-10-05 14:45:01 -0300

    feat: add a hello message to / route

--- src/app.ts
+++ src/app.ts
@@ -29,4 +29,8 @@ app.get("/students/random", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send('Hello!')
+})
+
 export default app;
